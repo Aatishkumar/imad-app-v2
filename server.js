0f523fa9507de333a658452e18bd29f36ce499a0
Aatishkumar <mm15b001@smail.iitm.ac.in>
2017-05-25 17:06:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.grt('/article-one',function(req, res){
-    res.sendFile("Article one is requested and will be served here");
-});
-app.grt('/article-two',function(req, res){
-    res.send("Article two is requested and will be served here");
-});
-app.grt('/article-three',function(req, res){
-    res.send("Article three is requested and will be served here");
-});
+// app.grt('/article-one',function(req, res){
+//     res.sendFile("Article one is requested and will be served here");
+// });
+// app.grt('/article-two',function(req, res){
+//     res.send("Article two is requested and will be served here");
+// });
+// app.grt('/article-three',function(req, res){
+//     res.send("Article three is requested and will be served here");
+// });
 
 app.get('/ui/style.css', function (req, res) {
   res.send(path.join(__dirname, 'ui', 'style.css'));
